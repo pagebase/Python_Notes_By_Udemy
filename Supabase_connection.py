f9b4823a321b8f1d39aa0aaf7b34c046830ec815
pagebase <163742860+pagebase@users.noreply.github.com>
2025-07-26 22:43:40 +0530

    Update Supabase_connection.py

--- Supabase_connection.py
+++ Supabase_connection.py
@@ -1,7 +1,7 @@
 from supabase import create_client, Client
 
-url: str = "https://omvilaxadaowyogomizr.supabase.co"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tdmlsYXhhZGFvd3lvZ29taXpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NDU5NjIsImV4cCI6MjA2OTEyMTk2Mn0.DCy09VioZIO-6W68ETAYeV7nnTWbt1uYDyolmR7CSvc"
+url= "https://omvilaxadaowyogomizr.supabase.co"
+key= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tdmlsYXhhZGFvd3lvZ29taXpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NDU5NjIsImV4cCI6MjA2OTEyMTk2Mn0.DCy09VioZIO-6W68ETAYeV7nnTWbt1uYDyolmR7CSvc"
 
 supabase: Client = create_client(url, key)
 
